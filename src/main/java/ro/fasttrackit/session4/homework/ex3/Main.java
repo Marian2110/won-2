fea9d9eb2f5076f840cb8b933d60bb566f826e37
Marian Datcu <marian.datcu@accenture.com>
2022-06-05 14:10:40 +0300

    session 4 homework

--- src/main/java/ro/fasttrackit/session4/homework/ex3/Main.java
+++ src/main/java/ro/fasttrackit/session4/homework/ex3/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         Person myPerson = new Person("John", 30, "Rome");
         Map<Predicate<Person>, Function<Person, String>> cases = Map.of(
-                p -> p.age() > 18, p -> "major",
+                p -> p.age() >=18, p -> "major",
                 p -> p.age() < 18, p -> "minor",
                 p -> p.address().equals("Rome"), p -> "From Rome",
                 p -> true, p -> "default"
